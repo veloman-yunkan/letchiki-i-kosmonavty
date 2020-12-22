d90ab967fd524a76566aee7c1c74ad487e362da7
Veloman Yunkan <veloman.yunkan@gmail.com>
2020-12-22 18:02:48 +0400

    Added original content from militera.lib.ru
    Added original HTML content from
    http://militera.lib.ru/memo/russian/kamanin_np/index.html

---
+++ orig_html/app.js
@@ -0,0 +1,20 @@
+dw_Tooltip.defaultProps = {
+    hoverable: true
+}
+dw_Tooltip.content_vars = {
+
+L1: '<!-- app_item --><div class=app_item><a name=1><span class=a>{1}ЦГАСА, ф. 67, оп. 10, д. 69.</div><!-- xapp_js -->',  
+
+L2: '<!-- app_item --><div class=app_item><a name=2><span class=a>{2}"Самолет", 1925, № 8, стр. 2.</div><!-- xapp_js -->',  
+
+L3: '<!-- app_item --><div class=app_item><a name=3><span class=a>{3}<i>М. В. Фрунзе.</i>  Избранные произведения, т. 2. Воениздат, 1957, стр. 289.</div><!-- xapp_js -->',  
+
+L4: '<!-- app_item --><div class=app_item><a name=4><span class=a>{4}"Вестник Воздушного флота", № 10, 1926, стр. 45 — 46.</div><!-- xapp_js -->',  
+
+L5: '<!-- app_item --><div class=app_item><a name=5><span class=a>{5}<i>В. И. Ленин.</i>  Полн. собр. соч., т. 51, стр. 43 — 44.</div><!-- xapp_js -->',  
+
+L6: '<!-- app_item --><div class=app_item><a name=6><span class=a>{6}ГИРД — Группа изучения реактивного движения, созданная при Центральном совете Осоавиахима в 1931 году.</div><!-- xapp_js -->'
+
+}
+
+<!-- bottom -->
